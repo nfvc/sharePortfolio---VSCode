6bf2bcbee5a777504cabc6f311f7022356a2fc77
adriano <adriano28ponyori@gmail.com>
2025-05-20 15:20:46 +0200

    feat : Création de la classe Client

---
+++ src/main/java/fr/utc/miage/shares/Client.java
@@ -0,0 +1,72 @@
+package fr.utc.miage.shares;
+
+public class Client {
+
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String telephone;
+    private String mail;
+    private String password;
+
+
+    /// Constructeur
+    public Client(String nom, String prenom, String adresse, String mail, String telephone, String password) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.mail = mail;
+        this.telephone = telephone;
+        this.password = password;
+    }
+
+
+    /// Méthodes
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
