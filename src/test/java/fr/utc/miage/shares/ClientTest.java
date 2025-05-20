cc7a3ea370abd6d77620f487936f5ba97793a4a6
adriano <adriano28ponyori@gmail.com>
2025-05-20 16:11:12 +0200

    feat : Modification des tests dans ClientTest

--- src/test/java/fr/utc/miage/shares/ClientTest.java
+++ src/test/java/fr/utc/miage/shares/ClientTest.java
@@ -3,60 +3,106 @@ package fr.utc.miage.shares;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.naming.InsufficientResourcesException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClientTest {
 
-    private Client client;
+    private static final String NOM = "Dupont";
+    private static final String PRENOM = "Jean";
+    private static final String ADRESSE = "3 rue manufacture";
+    private static final String MAIL = "jean.dupont@email.com";
+    private static final String TELEPHONE = "0123456789";
+    private static final String PASSWORD = "password123";
 
-    @BeforeEach
-    void setUp() {
-        Client client = new Client("Dupont", "Jean", "3 rue manufacture", "jean.dupont@email.com", "0123456789", "password123");
-    }
+
+    private static final String NOM2 = "Martin";
+    private static final String PRENOM2 = "Luc";
+    private static final String ADRESSE2 = "2 rue arsenal";
+    private static final String MAIL2 = "luc.martin@email.com";
+    private static final String TELEPHONE2 = "0987654321";
+    private static final String PASSWORD2 = "newPassword456";
+
+
+
 
     @Test
-    void testConstructorAndGetters() {
-        assertEquals("Dupont", client.getNom());
-        assertEquals("Jean", client.getPrenom());
-        assertEquals("3 rue manufacture", client.getAdresse());
-        assertEquals("jean.dupont@email.com", client.getMail());
-        assertEquals("0123456789", client.getTelephone());
-        assertEquals("password123", client.getPassword());
+    void testConstructor() {
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String nom_attendu = "Dupont";
+        final String prenom_attendu = "Jean";
+        final String adresse_attendu = "3 rue manufacture";
+        final String mail_attendu = "jean.dupont@email.com";
+        final String tel_attendu = "0123456789";
+        final String password_attendu = "password123";
+
+        assertEquals(nom_attendu, client.getNom());
+        assertEquals(prenom_attendu, client.getPrenom());
+        assertEquals(adresse_attendu, client.getAdresse());
+        assertEquals(mail_attendu, client.getMail());
+        assertEquals(tel_attendu, client.getTelephone());
+        assertEquals(password_attendu, client.getPassword());
     }
 
     @Test
     void testSetNom() {
-        client.setNom("Martin");
-        assertEquals("Martin", client.getNom());
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String nom_attendu = NOM2;
+        client.setNom(NOM2);
+
+        assertEquals(nom_attendu, client.getNom());
     }
 
     @Test
     void testSetPrenom() {
-        client.setPrenom("Luc");
-        assertEquals("Luc", client.getPrenom());
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String prenom_attendu = PRENOM2;
+        client.setPrenom(PRENOM2);
+
+        assertEquals(prenom_attendu, client.getPrenom());
     }
 
     @Test
     void testSetAdresse() {
-        client.setAdresse("2 rue arsenal");
-        assertEquals("2 rue arsenal", client.getAdresse());
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String adresse_attendu = ADRESSE2;
+        client.setAdresse(ADRESSE2);
+
+        assertEquals(adresse_attendu, client.getAdresse());
     }
 
     @Test
     void testSetMail() {
-        client.setMail("luc.martin@email.com");
-        assertEquals("luc.martin@email.com", client.getMail());
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String mail_attendu = MAIL2;
+        client.setMail(MAIL2);
+
+        assertEquals(mail_attendu, client.getMail());
     }
 
     @Test
     void testSetTelephone() {
-        client.setTelephone("0987654321");
-        assertEquals("0987654321", client.getTelephone());
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String tel_attendu = TELEPHONE2;
+        client.setTelephone(TELEPHONE2);
+
+        assertEquals(tel_attendu, client.getTelephone());
     }
 
     @Test
     void testSetPassword() {
-        client.setPassword("newPassword456");
-        assertEquals("newPassword456", client.getPassword());
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String mdp_attendu = PASSWORD2;
+        client.setPassword(PASSWORD2);
+
+        assertEquals(mdp_attendu, client.getPassword());
     }
 }
