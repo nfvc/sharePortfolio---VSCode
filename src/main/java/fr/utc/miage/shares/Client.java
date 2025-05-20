061bb634eac45b78bb0d056070e5a1f9f272fa82
Adriano2802 <adriano28ponyori@gmail.com>
2025-05-20 16:21:58 +0200

    Feature Client : Création de la classe Client (#31)
    feat : creation des classes Client et ClientTest

---
+++ src/main/java/fr/utc/miage/shares/Client.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2025 David Navarre &lt;David.Navarre at irit.fr&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.utc.miage.shares;
+
+public class Client {
+
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String telephone;
+    private String mail;
+    private String password;
+
+
+    /// Constructeur
+    public Client(String nom, String prenom, String adresse, String mail, String telephone, String password) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.mail = mail;
+        this.telephone = telephone;
+        this.password = password;
+    }
+
+
+    /// Méthodes
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/fr/utc/miage/shares/ClientTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2025 David Navarre &lt;David.Navarre at irit.fr&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.utc.miage.shares;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.naming.InsufficientResourcesException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ClientTest {
+
+    private static final String NOM = "Dupont";
+    private static final String PRENOM = "Jean";
+    private static final String ADRESSE = "3 rue manufacture";
+    private static final String MAIL = "jean.dupont@email.com";
+    private static final String TELEPHONE = "0123456789";
+    private static final String PASSWORD = "password123";
+
+
+    private static final String NOM2 = "Martin";
+    private static final String PRENOM2 = "Luc";
+    private static final String ADRESSE2 = "2 rue arsenal";
+    private static final String MAIL2 = "luc.martin@email.com";
+    private static final String TELEPHONE2 = "0987654321";
+    private static final String PASSWORD2 = "newPassword456";
+
+
+
+
+    @Test
+    void testConstructor() {
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String nom_attendu = "Dupont";
+        final String prenom_attendu = "Jean";
+        final String adresse_attendu = "3 rue manufacture";
+        final String mail_attendu = "jean.dupont@email.com";
+        final String tel_attendu = "0123456789";
+        final String password_attendu = "password123";
+
+        assertEquals(nom_attendu, client.getNom());
+        assertEquals(prenom_attendu, client.getPrenom());
+        assertEquals(adresse_attendu, client.getAdresse());
+        assertEquals(mail_attendu, client.getMail());
+        assertEquals(tel_attendu, client.getTelephone());
+        assertEquals(password_attendu, client.getPassword());
+    }
+
+    @Test
+    void testSetNom() {
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String nom_attendu = NOM2;
+        client.setNom(NOM2);
+
+        assertEquals(nom_attendu, client.getNom());
+    }
+
+    @Test
+    void testSetPrenom() {
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String prenom_attendu = PRENOM2;
+        client.setPrenom(PRENOM2);
+
+        assertEquals(prenom_attendu, client.getPrenom());
+    }
+
+    @Test
+    void testSetAdresse() {
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String adresse_attendu = ADRESSE2;
+        client.setAdresse(ADRESSE2);
+
+        assertEquals(adresse_attendu, client.getAdresse());
+    }
+
+    @Test
+    void testSetMail() {
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String mail_attendu = MAIL2;
+        client.setMail(MAIL2);
+
+        assertEquals(mail_attendu, client.getMail());
+    }
+
+    @Test
+    void testSetTelephone() {
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String tel_attendu = TELEPHONE2;
+        client.setTelephone(TELEPHONE2);
+
+        assertEquals(tel_attendu, client.getTelephone());
+    }
+
+    @Test
+    void testSetPassword() {
+        final Client client = new Client(NOM,PRENOM,ADRESSE,MAIL,TELEPHONE,PASSWORD);
+
+        final String mdp_attendu = PASSWORD2;
+        client.setPassword(PASSWORD2);
+
+        assertEquals(mdp_attendu, client.getPassword());
+    }
+}
